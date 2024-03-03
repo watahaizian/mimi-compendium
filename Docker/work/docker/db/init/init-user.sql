467161e22f49ba008ff031dfac31470e4fbc0396
watahaizian <watahaizian@gmail.com>
2024-03-03 19:52:58 +0900

    add init

---
+++ Docker/work/docker/db/init/init-user.sql
@@ -0,0 +1,11 @@
+create table user (id int, name varchar(32), email varchar(32));
+
+insert into
+    user (id, name, email)
+values
+    (1, 'John Doe', 'john@example.com');
+
+insert into
+    user (id, name, email)
+values
+    (2, 'Jane Doe', 'jane@example.com');
